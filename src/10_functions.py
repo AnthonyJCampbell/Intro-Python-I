59b154c3e2b0ba2712a7dae6d840f0ba1a44f1fb
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-09-30 15:25:01 +0200

    Exercise 11: args

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,6 +1,5 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
-# YOUR CODE HERE
 def is_even(num):
   if (num % 2 == 0):
     return True 
@@ -14,5 +13,4 @@ if (is_even(num)):
   print("Even!")
 else:
   print("Odd")
-# YOUR CODE HERE
 
